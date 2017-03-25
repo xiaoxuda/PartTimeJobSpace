e1b84641c5c36a20478fc1b8365663004207a606
li-yz <1010873855@qq.com>
2017-03-25 18:03:56 +0800

    用户注册登录

--- TestManagerApp/src/main/java/cn/orditech/controller/UserController.java
+++ TestManagerApp/src/main/java/cn/orditech/controller/UserController.java
@@ -1,13 +1,18 @@
 package cn.orditech.controller;
 
+import cn.orditech.entity.User;
 import cn.orditech.service.UserService;
 import com.sun.deploy.net.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by Administrator on 2017/3/25.
@@ -16,14 +21,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class UserController {
     @Autowired
     private UserService userService;
-
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @RequestMapping(path = "/register",method = {RequestMethod.GET,RequestMethod.POST})
     @ResponseBody
     public String register(@RequestParam(value = "account",required = false)String account,
                            @RequestParam(value = "password",required = false)String password,
-                           HttpResponse response){
-
-        return "";
+                           @RequestParam(value = "name",required = false)String name,
+                           HttpServletResponse response){
+        try{
+        User user = new User();
+        user.setAcount(account);
+        user.setPassword(password);
+        user.setName(name);
+        userService.insert(user);
+        return "注册成功";
+        }catch (Exception e){
+            logger.info("注册异常",e.getMessage());
+            return "注册失败";
+        }
     }
 }
