19874528c275367d0078b19bc3a7289b38edf14c
xiaoxuda <xiaoxuda0715@163.com>
2018-03-18 14:04:56 +0800

    删除无用文件

--- mytoutiao/src/main/java/com/jlu/model/EntityType.java
+++
@@ -1,9 +0,0 @@
-package com.jlu.model;
-
-/**
- * Created by Administrator on 2016/7/19.
- */
-public class EntityType {
-    public static int ENTITY_TYPE_NEWS = 1;
-    public static int ENTITY_TYPE_COMMENT = 2;
-}
